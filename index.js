578e3d711ae3540a705b8f5147629cd3800f0896
danishmalik102 <danishimran108@gmail.com>
2023-12-06 12:43:42 +0500

    Setting up post routes

--- index.js
+++ index.js
@@ -51,6 +51,7 @@ const upload = multer({ storage });
 
 /* ROUTES WITH FILES */
 app.post("/auth/register", upload.single("picture"), register);
+app.post ("/posts", verifyToken, upload.single("picture"), createpost);
 /* ROUTES */
 app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
